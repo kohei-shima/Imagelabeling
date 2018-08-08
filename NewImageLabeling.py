f1a170b0131135bee8bfd35da633bf852d0ee46e
kohei-shima <40019082+kohei-shima@users.noreply.github.com>
2018-08-08 17:04:36 +0900

    Add files via upload

--- NewImageLabeling.py
+++ NewImageLabeling.py
@@ -3,38 +3,30 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-#def show_dialog():
-#    print('ok')
-#    fTyp = [("","*")]
-#    iDir = os.path.abspath(os.path.dirname(__file__))
-#    filepath =  filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+#rootの作成
+root = Tk()
+root.title("ImageLabeling")
+root.geometry("500x500")
 
-#=================================================
-# main function
-#=================================================
-if __name__ == '___main__':
-    #rootの作成
-    root = Tk()
-    root.title("ImageLabeling")
-
-    #メニューバー
-    menu = tk.Menu(root)
-    root.config(menu=menu)
-
-    filemenu = tk.Menu(menu)
-    menu.add_cascade(label="ファイル",menu=filemenu)
-    filemenu.add_command(label="フォルダを開く", command=show_dialog)
+def show_dialog():
 
     #ファイルを開く
-    #path = 'C:/Users/254994/Desktop/Image_labeling/ikuchan/'
-    #ImgName = 'ikuchan.'
-    #ext = 'jpg'
+    fTyp = [("","*.jpg;*.bmp")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    filepath =  filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
 
     #画象表示
     Pic_Image = ImageTk.PhotoImage(Image.open(filepath))
-    #Pic_Image = ImageTk.PhotoImage(Image.open(path + ImgName + ext))
     Pic_Label = Label(root, image = Pic_Image)
+    print("ok")
     Pic_Label.pack()
 
-    #イベントループ
-    root.mainloop()
+#メニューバー
+menu = Menu(root)
+root.config(menu=menu)
+
+filemenu = Menu(menu,tearoff=False)
+menu.add_cascade(label="ファイル",menu=filemenu)
+filemenu.add_command(label="フォルダを開く", command=show_dialog)
+
+root.mainloop()
